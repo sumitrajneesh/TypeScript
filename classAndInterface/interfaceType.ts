28b5eed5c1ea8f6255308a0162227a5fe7068044
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2020-07-28 12:20:59 +0530

    commit

--- classAndInterface/interfaceType.ts
+++ classAndInterface/interfaceType.ts
@@ -1,15 +1,42 @@
-interface Person{
+
+// interface Person{
+//     name:string;
+//     age:number;
+//     greet(phrase:string): void;
+// }
+
+
+// let user1:Person;
+// user1  = {
+//     name:'Sumit',
+//     age:30,
+//     greet(phrase:string){
+//         console.log(phrase + ' ' + this.name )
+//     }
+// }
+
+/***
+ * Using interface with class
+ */
+interface Greetable {
     name:string;
-    age:number;
-    greet(phrase:string): void;
+    greet(phrase:string):void;
 }
 
-
-let user1:Person;
-user1  = {
-    name:'Sumit',
-    age:30,
+class Person implements Greetable {
+    name:string;
+    age:30;
+    constructor(n:string){
+        this.name = n;
+            
+    }
     greet(phrase:string){
         console.log(phrase + ' ' + this.name)
     }
+
 }
+
+let user1:Greetable;
+user1 = new Person('Sumit');
+user1.greet('HI there - I am');
+console.log(user1)
